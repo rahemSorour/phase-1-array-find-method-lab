8a9f9ac5c76a0ae4f9e74341ec985e605044170e
rahem sorour <sorourrahem17@gmail.com>
2023-08-11 20:58:53 +0100

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(array) {
+    const findwinner = array.find(function(winner) {
+      return winner.result === "W"; });
+  
+    if (findwinner) {
+      return findwinner.year;
+    } else {return undefined}
+  }
+
+  
+arry=[{year:"1999",result:"lin"},
+{year: "2018", result: "W"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}]
+
+console.log(superbowlWin(arry))
